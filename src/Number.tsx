ae4ed953b85cfbe0738d5e29f89fde612acd51bb
Gomdi <ginpa0886@gmail.com>
2021-02-05 00:20:39 +0900

    #8.8 Styled Components and Typescript part Two

--- src/Number.tsx
+++ src/Number.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 
 
+
 const Container = styled.span<{isBlue:boolean}>`
-  color:${props => (props.isBlue ? "blue" : "black")};
+  color:${props => (props.isBlue ? props.theme.blueColor : "black")};
 `;
 
 interface IProps{
   count: number;
 }
 
-const Number: React.FunctionComponent<IProps> = ({count}) => <Container isBlue={count > 10}>{count}</Container>
+const Number: React.FunctionComponent<IProps> = ({count}) => (<Container isBlue={count > 10}>{count}</Container>);
 
 export default Number;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ThemeProvider } from 'styled-components';
+import theme from './theme';
 
-const plus = (a:string, b:string) => a + b;
+// const plus = (a:string, b:string) => a + b;
 
 
 ReactDOM.render(
-    <App />,
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+    ,
   document.getElementById('root')
 );
 
