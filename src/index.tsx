25d3845b2fe46f130970e2ef7540e03b0d782aac
Gomdi <ginpa0886@gmail.com>
2021-02-04 16:38:56 +0900

    #8.3 Typescript and React Introduction

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const plus = (a:string, b:string) => a + b;
+
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
+
+
